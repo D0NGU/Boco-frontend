1fc84044e6b969af8207821c62fdccbb93ba1a0c
Oskar Eidem <oskareid@stud.ntnu.no>
2022-04-20 13:23:06 +0200

    revert

---
+++ src/views/notFound.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>404 Not Found</h1>
+  <ErrorBox />
+</template>
+
+<script>
+
+import ErrorBox from '../components/Errorbox.vue'
+
+export default {
+    name: 'NotFound',
+    components: {ErrorBox}
+
+}
+</script>
+
+<style>
+
+</style>
