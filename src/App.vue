0003537d0877a9cb86b8d9af7e7e9df41db18127
Minh Dan Nguyen <minhdn@stud.ntnu.no>
2022-04-20 16:19:23 +0200

    created a sort and search box

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <v-app>
+    <v-main>
+      <nav>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+      </nav>
+      <sort-and-search />
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
 
 <style>
@@ -28,3 +33,9 @@ nav a.router-link-exact-active {
   color: #42b983;
 }
 </style>
+<script>
+import SortAndSearch from "@/components/sortAndSearch";
+export default {
+  components: {SortAndSearch}
+}
+</script>
