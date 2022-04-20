62f63f18a32db67e474e449c9da6ee9eb13eba01
Henriette Brekke Sunde <henriesu@stud.ntnu.no>
2022-04-20 15:17:38 +0200

    Added code to components/Listing.vue and created views/Listing.vue

--- src/components/Listing.vue
+++ src/components/Listing.vue
@@ -1,12 +1,40 @@
 <!-- En "listing" instans. (En annonseboks) -->
 
 <template>
-  
+  <h1 id="createListingHeadline">Opprett en ny annonse</h1>
+  <form class="adForm">
+    <label>Navn på annonse</label>
+    <input
+        v-model="adName"
+        type="text"
+        placeholder="Navn på annonse"
+    />
+    <label>Beskrivelse</label>
+    <input
+        v-model="adDescription"
+        type="text"
+        placeholder="Beskrivelse"
+    />
+    <label>Bilder</label>
+    <v-file-input
+        :rules="rules"
+        placeholder="Last opp bildene"
+        accept="image/*"
+        multiple
+        prepend-icon="mdi-camera"
+    />
+  </form>
+
 </template>
 
 <script>
 export default {
-
+  name: "Listing",
+  data: () => ({
+    rules: [
+      value => !value || value.size < 2000000 || 'Image size should be less than 2 MB!',
+    ],
+  }),
 }
 </script>
 
