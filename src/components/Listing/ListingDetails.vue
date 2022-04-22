6c93f6900fe29baf28e843a5ce1981473df92ddf
Minh Dan Nguyen <minhdn@stud.ntnu.no>
2022-04-22 11:56:45 +0200

    Added error message and some description when requesting rental

--- src/components/Listing/ListingDetails.vue
+++ src/components/Listing/ListingDetails.vue
@@ -14,32 +14,38 @@
           <v-img src="https://kvener.no/wp-content/uploads/2019/02/blank-profile-picture-973460_640.png" alt="profile picture"></v-img>
         </v-avatar> Per Hansen</p>
       <v-divider style="margin: 10px"/>
-      <Datepicker range></Datepicker>
     </div>
-    <v-btn id="requestBtn" @click="sendRequest"> Send Forespørsel </v-btn>
+    <div id="requestForm">
+      <v-alert type="error" v-if="invalidDate">Du må legge til en dato</v-alert>
+      <p>Interessert i å leie gjenstanden? Legg til ønsket dato og send en forespørsel!</p>
+      <Datepicker range v-model="date" :enableTimePicker="false" showNowButton></Datepicker>
+      <v-btn id="requestBtn" @click="sendRequest"> Send Forespørsel </v-btn>
+    </div>
   </div>
 </template>
 
 <script>
 import Datepicker from '@vuepic/vue-datepicker';
 import '@vuepic/vue-datepicker/dist/main.css'
+import { ref } from 'vue';
 
 export default {
   name: "ListingDetails",
   components: {Datepicker},
   data () {
     return {
-      picker: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+      date: ref(),
+      invalidDate: false,
     }
-  },
-  computed: {
-    dateRangeText () {
-      return this.dates.join(' ~ ')
-    },
   },
   methods: {
     sendRequest() {
-
+      if(this.date !== undefined && this.date !== null){
+        //TODO SEND FORESPØRSEL
+      } else {
+        this.invalidDate = true;
+        setTimeout(() => window.scrollTo(0, document.body.scrollHeight), 1);
+      }
     },
   },
 }
@@ -52,5 +58,8 @@ export default {
 }
 #requestBtn {
   margin: 0 auto 20px auto;
+}
+#requestForm > *{
+  padding: 10px;
 }
 </style>
