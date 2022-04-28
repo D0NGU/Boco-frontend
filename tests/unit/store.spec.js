2fc92319da3e6933cc852de0bee896a3b9b9f8c0
Mathangi Pushparajah <mathangp@stud.ntnu.no>
2022-04-28 12:36:01 +0200

    forgot to add file for store test

---
+++ tests/unit/store.spec.js
@@ -0,0 +1,65 @@
+import { mutations, actions, getters } from "@/store";
+import {createStore} from "vuex";
+
+describe('testing store', () => {
+
+    it('Test SET_STATUS mutation in store', () => {
+        const state = {
+            loggedIn: false,
+        }
+        mutations.SET_STATUS(state, true)
+        expect(state.loggedIn).toBe(true)
+    });
+
+    it('Test SET_MYUSERID mutation in store', () => {
+        const state = {
+            myUserId: '',
+        }
+        mutations.SET_MYUSERID(state, 4)
+        expect(state.myUserId).toBe(4)
+    });
+
+    it('Test SET_TOKEN mutation in store', () => {
+        const state = {
+            token: '',
+        }
+        mutations.SET_TOKEN(state, "testtoken")
+        expect(state.token).toBe("testtoken")
+    });
+
+    it('Test token getter in store', () => {
+        const state = {
+            loggedIn: false,
+            myUserId: 3,
+            token: 'testing',
+        }
+        const getToken = getters.token(state)
+        expect(getToken).toBe("testing")
+    });
+
+    it('Test myUserId getter in store', () => {
+        const state = {
+            loggedIn: false,
+            myUserId: 3,
+            token: 'testing',
+        }
+        const getUserId = getters.myUserId(state)
+        expect(getUserId).toBe(3)
+    });
+
+    it('Test login action in store', () => {
+        const testStore = createStore({
+            state: {
+                loggedIn: false,
+                myUserId: '',
+                token: '',
+            },
+            mutations: mutations
+        })
+
+        actions.login(testStore,{token: "token", userID: 1});
+        expect(testStore.state.loggedIn).toBe(true);
+
+    });
+
+});
