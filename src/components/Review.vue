bafb95f4d55ed3d47e68b5ee071bd8efa88a2f03
Henriette Brekke Sunde <henriesu@stud.ntnu.no>
2022-04-29 11:38:09 +0200

    New branch

---
+++ src/components/Review.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Legg igjen din anmeldelse her</h1>
+  <v-rating></v-rating>
+  <v-text-field
+      label="Kommentar">
+
+  </v-text-field>
+
+</template>
+
+<script>
+export default {
+  name: "Review"
+}
+</script>
+
+<style scoped>
+
+</style>
