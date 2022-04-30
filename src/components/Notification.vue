b1d88172fe2a46c54a41c70ec8ff393d879bc389
joelmt <joelmt@stud.ntnu.no>
2022-04-30 18:42:23 +0200

    Implemented notification component and api calls for notifications

--- src/components/Notification.vue
+++ src/components/Notification.vue
@@ -12,76 +12,16 @@
       <v-col cols="8"
       sm="4"
       md="8">
-        <p class="notifText">Du har fått en anmeldelse</p>
-        <p class="text-caption dateText">Fre 29. April 12:00</p>
+        <p class="notifText">{{ description }}</p>
+        <p class="text-caption dateText">{{ alertDate }}</p>
       </v-col>
-    </v-row>
-  </v-card>
-
-  <v-card class="notifCard">
-    <v-row no-gutters=""
-           align="center"
-           justify="center">
-      <v-col
-          cols="2"
-          md="2">
-        <v-icon class="notifIcon"> mdi-newspaper-plus </v-icon>
-      </v-col>
-      <v-col cols="8"
-             sm="4"
-             md="8">
-        <p class="notifText">Du har fått en forespørsel</p>
-      </v-col>
-    </v-row>
-  </v-card>
-
-  <v-card class="notifCard">
-    <v-row no-gutters=""
-           align="center"
-           justify="center">
-      <v-col
-          cols="2"
-          md="2">
-        <v-icon class="notifIcon"> mdi-newspaper-minus </v-icon>
-      </v-col>
-      <v-col cols="8"
-             sm="4"
-             md="8">
-        <p class="notifText"> Leien av et produkt har blitt avlyst </p>
-      </v-col>
-    </v-row>
-  </v-card>
-
-  <v-card class="notifCard">
-    <v-row no-gutters=""
-           align="center"
-           justify="center">
-      <v-col
-          cols="2"
-          md="2">
-        <v-icon class="notifIcon"> mdi-newspaper-plus </v-icon>
-      </v-col>
-      <v-col cols="8"
-             sm="4"
-             md="8">
-        <p class="notifText"> Utleier har godkjent forespørselen din </p>
-      </v-col>
-    </v-row>
-  </v-card>
-
-  <v-card class="notifCard">
-    <v-row no-gutters=""
-           align="center"
-           justify="center">
-      <v-col
-          cols="2"
-          md="2">
-        <v-icon class="notifIcon"> mdi-newspaper-minus </v-icon>
-      </v-col>
-      <v-col cols="8"
-             sm="4"
-             md="8">
-        <p class="notifText">Utleier har avslått forespørselen din</p>
+      <v-col>
+        <v-btn v-show="this.hasSeen" @click="markAsSeen(this.alertId)">
+          Har sett
+        </v-btn>
+        <v-btn @click="deleteAlert(this.alertId)">
+          Slett
+        </v-btn>
       </v-col>
     </v-row>
   </v-card>
@@ -89,13 +29,32 @@
 </template>
 
 <script>
+import {getApiClient} from "@/service/ApiService";
+
 export default {
   name: "Notification",
+  props: {
+    alertId: Number,
+    description: String,
+    alertDate: Date,
+    hasSeen: Boolean,
+    optionalId: Number,
+    userId: Number
+  },
   data () {
     return {
       dialog: false,
     }
   },
+  methods: {
+    async deleteAlert(alertId) {
+      await getApiClient.delete("/alerts/" + alertId)
+      await this.getAlerts()
+    },
+    async markAsSeen(alertId) {
+      await getApiClient.put("/alerts/seen/" + alertId)
+    }
+  }
 }
 </script>
 
--- src/components/NotificationView.vue
+++ src/components/NotificationView.vue
@@ -1,17 +1,36 @@
 <template>
-  <Notification />
+  <div v-for="alert in alerts">
+    <Notification
+        :alertId="alert.alertId"
+        :description="alert.description"
+        :alertDate="alert.alertDate"
+        :hasSeen="alert.hasSeen"
+        :optionalId="alert.optionalId"
+        :userId="alert.userId"
+    />
+  </div>
 </template>
 
 <script>
 import Notification from "@/components/Notification";
+import {getApiClient} from "@/service/ApiService";
 export default {
   name: "NotificationView",
   components: {Notification},
   data () {
     return {
       dialog: false,
+      alerts: []
     }
   },
+  methods: {
+    async getAlerts() {
+      this.alerts = (await getApiClient.get("/alerts/user/" + this.$store.getters.myUserId)).data;
+    },
+  },
+  beforeMount() {
+    this.getAlerts()
+  }
 }
 </script>
 
