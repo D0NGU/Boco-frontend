beaf5c22b116edf8c1fda7d373566a40e47458bd
Oskar Eidem <oskareid@stud.ntnu.no>
2022-05-02 10:32:06 +0200

    PWA themeColor

--- vue.config.js
+++ vue.config.js
@@ -7,4 +7,9 @@ module.exports = defineConfig({
       // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
     },
   },
+
+  pwa: {
+    name: "BoCo",
+    themeColor: "#004AABFF"
+  }
 })
