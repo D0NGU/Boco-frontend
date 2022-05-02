1203a8b47cffda01794a4c85ee04a2897e37d338
Henriette Brekke Sunde <henriesu@stud.ntnu.no>
2022-05-02 10:36:55 +0200

    Simple footer, problem including image from local repository

---
+++ src/components/Misc/Footer.vue
@@ -0,0 +1,37 @@
+<template>
+    <v-row id="footer" justify="center" no-gutters="">
+      <v-col cols="12">
+        <h3>BoCo</h3>
+        <v-divider color="white"/>
+        <h4 onclick="location.href='/Home'">Kontakt oss</h4>
+      </v-col>
+      <!--<v-col id="date" cols="12">
+        <h5>Laget av</h5>
+        Scrum-team 08 -
+        {{ new Date().getFullYear() }}
+      </v-col>-->
+    </v-row>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  data () {
+    return {
+
+    }
+  },
+}
+</script>
+
+<style scoped>
+#footer{
+  padding: 1em;
+  background-color: var(--bocoBlue);
+  color: white;
+}
+#date{
+  padding-top: 1em;
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,14 +2,16 @@
 
 <template>
   <ListingView />
+  <Footer id="footer"/>
 </template>
 
 <script>
 import ListingView from "@/components/Listing/ListingView";
 import UserAccountService from "@/service/UserAccountService";
+import Footer from "@/components/Misc/Footer";
 export default {
     name: 'Home',
-  components: {ListingView},
+  components: {Footer, ListingView},
   async beforeMount() {
     const userInfo = (await UserAccountService.getUserId(this.$store.state.email)).data
     this.$store.commit("SET_MYUSERID", userInfo.id)
@@ -18,5 +20,13 @@ export default {
 </script>
 
 <style>
-
+#footer{
+  position: -webkit-sticky;
+  position: sticky;
+  display: flex;
+  z-index: 1;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+}
 </style>
