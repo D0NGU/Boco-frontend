0c3c4572386329542f5a5d3a5a38aba7c92a01b0
Henriette Brekke Sunde <henriesu@stud.ntnu.no>
2022-05-02 15:46:11 +0200

    Implemented simple contactform and added routing

---
+++ src/components/Contact.vue
@@ -0,0 +1,60 @@
+<template>
+  <h1>Kontakt oss</h1>
+  <v-text-field
+      v-model="fname"
+      label="Fornavn"
+      type="text"
+      hide-details="auto"
+  ></v-text-field>
+  <v-text-field
+      v-model="lname"
+      label="Etternavn"
+      type="text"
+      hide-details="auto"
+  ></v-text-field>
+  <v-text-field
+      v-model="email"
+      label="E-post"
+      type="text"
+      hide-details="auto"
+  ></v-text-field>
+  <v-text-field
+      v-model="feedback"
+      label="Kommentar"
+      type="text"
+      hide-details="auto"
+      height="100px"
+  ></v-text-field>
+  <v-btn
+      id="sendForm"
+  >Send kontaktskjema</v-btn>
+  <v-btn
+      id="dismiss"
+  >Avbryt</v-btn>
+</template>
+
+<script>
+export default {
+  name: "Contact",
+  data () {
+    return {
+      fname: '',
+      lname: '',
+      email: '',
+      feedback: '',
+    }
+  }
+}
+</script>
+
+<style scoped>
+#sendForm{
+  background-color: var(--bocoBlue);
+  color: white;
+  font-weight: bold;
+}
+#dismiss{
+  color: var(--bocoBlue);
+  font-weight: bold;
+}
+</style>
