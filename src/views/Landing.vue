d0e767d9f16c892c5ae557faa4d3c0f24067ffd6
Minh Dan Nguyen <minhdn@stud.ntnu.no>
2022-05-04 00:32:05 +0200

    made a landing page

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,13 +1,29 @@
-<template>
-  <div>
-    <v-img ></v-img>
-    <h1>Boco</h1>
-    <h3>for å skape et bærekraftigere samfunn</h3>
-    <v-btn>Lei et produkt</v-btn>
-
+<template v-cloak>
+  <div id="cover">
+    <div id="imageCover"></div>
+    <img src="/img/flowers.jpg" alt="picture" id="coverImage">
+    <div id="coverText">
+    <h1>Borrow <br> Community</h1>
+    <h3>- for å skape et
+      <br>bærekraftigere samfunn</h3>
+    <v-btn class="button" @click="$router.push({name: 'Login'})">Start nå</v-btn>
+    </div>
   </div>
-  <div>
-    <ListingView />
+  <div class="paragraph">
+    <h2>Hvem er vi?</h2>
+    <img src="/img/business.png" alt="" class="paraImage">
+    <p>Borrow Community (BoCo) er et norsk selskap som tilbyr en plattform for utlån av gjenstander for privatpersoner og bedrifter. </p>
+  </div>
+  <div class="paragraph">
+    <h2>Bedre for miljøet</h2>
+    <img src="/img/enviro.png" alt="" class="paraImage">
+    <p>Vi i BoCo ønsker å legge til rette for mer bærekraftige samfunn. Verden må bli mer sirkulær!</p>
+  </div>
+  <div class="paragraph">
+    <h2> - og lommeboka !</h2>
+    <img src="/img/wallet.webp" alt="" class="paraImage">
+    <p>Lån gjenstander av noen som allerede eier det, og som ønsker å leie ut.
+    Den som legger ut annonsen kan velge å ta betalt for utleie, eller leie ut gratis!</p>
   </div>
 </template>
 
@@ -21,5 +37,60 @@ export default {
 </script>
 
 <style scoped>
+#cover {
+  height: 100%;
+  width: 100%;
+  position: relative;
+  display: flex;
+}
 
+#coverImage {
+  height: 100%;
+  width: 100%;
+  flex: 1;
+}
+#imageCover {
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  background-color: rgba(0, 0, 0, 0.5);
+  flex: 1;
+}
+
+#coverText {
+  animation-fill-mode: forwards;
+  animation-name: title-fade;
+  animation-duration: 2s;
+  background-color: rgba(0, 0, 0, 0.5);
+  color: white;
+  padding: 15px;
+  border: 5px solid white;
+  width: 85%;
+  position: absolute;
+  top: 45%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+.button {
+  color: var(--bocoBlue);
+  margin-top: 20px;
+  font-weight: bold;
+}
+.paragraph {
+  text-align: center;
+  padding: 30px;
+}
+.paragraph:nth-child(2n) {
+  background-color: white;
+}
+.paraImage {
+  height: 100px;
+  margin: 20px;
+}
+
+@keyframes title-fade  {
+  from {opacity: 0}
+  to {opacity: 1}
+  100% {letter-spacing: 2.5px}
+}
 </style>
