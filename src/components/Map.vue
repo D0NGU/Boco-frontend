80ff65dc6feada1408e0e99eaada8f2ddac907e0
matssg <matssg@stud.ntnu.no>
2022-05-03 10:25:23 +0200

    Fix map styling

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -53,6 +53,11 @@ export default {
   top: 0; right: 0; bottom: 0; left: 0;
   position: fixed;
   background: rgba(0,0,0,0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  min-height: 100vh;
 }
 
 #map {
