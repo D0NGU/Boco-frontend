14801a1525e78741c40a39ddce69414f67fe2efb
Hanna Nordhus <hannagn@stud.ntnu.no>
2022-05-04 16:05:35 +0200

    Added descriptions of files

--- src/components/Listing/RentalRequest.vue
+++ src/components/Listing/RentalRequest.vue
@@ -1,3 +1,5 @@
+<!-- component for forespørsel siden -->
+
 <template>
   <v-card class="rounded-l rentalCard">
     <v-avatar x-large id="profilePicture"><v-img src="https://www.megaflis.no/globalassets/productimages/6952062643067_1.png?ref=1931F74161&w=1920&scale=both&mode=pad&h=1920&format=jpg"></v-img></v-avatar>
