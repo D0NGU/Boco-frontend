fdfc998a3bfde305fdf7bde2c47d01e3693a186f
Minh Dan Nguyen <minhdn@stud.ntnu.no>
2022-05-04 09:59:04 +0200

    removed unused import

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-//import Landing from '../views/Landing.vue'
 import Home from '../views/Home.vue'
 import Account from '../views/Account.vue'
 import NotFound from '../views/NotFoundPage.vue'
