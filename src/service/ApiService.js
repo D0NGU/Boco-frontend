ea72a3d3203fbb895b0d9aa91e5302a0408e8a01
Minh Dan Nguyen <minhdn@stud.ntnu.no>
2022-05-05 10:36:38 +0200

    hide ads that have availability window in the past

--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import VueCookie from 'vue-cookie'
+import store from '@/store'
 
 export const baseURL = `http://localhost:8080/api/`;
 //export const baseURL = `http://10.22.0.141:8080/api/`; //Endre til din ip hvis du skal teste på mobil
