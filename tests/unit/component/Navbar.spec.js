663213646d5cc7f37b038c998494e41df589f927
Oskar Eidem <oskareid@stud.ntnu.no>
2022-05-05 15:29:58 +0200

    test for listing input

--- tests/unit/component/Navbar.spec.js
+++ tests/unit/component/Navbar.spec.js
@@ -1,15 +1,19 @@
-import { shallowMount } from '@vue/test-utils'
+import { shallowMount, mount } from '@vue/test-utils'
 import Navbar from "@/components/Misc/Navbar";
 import {createStore} from "vuex";
 
 
 describe('Testing Navbar.vue', () => {
 
-    const wrapper = shallowMount(Navbar, {
+    const wrapper = mount(Navbar, {
         stubs: ['router-link', 'router-view'],
     });
 
-    it('Navbar has route to /home', () => {
+    /*
+        At render the Object points to a route in router.
+        if Object exists the navbar must point to valid routes
+     */
+    it('Navbar has routes', () => {
         expect(wrapper.html()).toContain("[object Object]");
     })
 
