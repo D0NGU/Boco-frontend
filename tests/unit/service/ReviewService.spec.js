e3a0d2395c90c7a1010a4e6fc73fef19f079c42e
matssg <matssg@stud.ntnu.no>
2022-05-06 11:34:02 +0200

    Add tests for "ReviewService.js"

---
+++ tests/unit/service/ReviewService.spec.js
@@ -0,0 +1,24 @@
+import ReviewService from '@/service/ReviewService'
+import { getApiClient } from "@/service/ApiService";
+
+beforeEach(() => 
+    jest.spyOn(getApiClient, 'post'), 
+    jest.spyOn(getApiClient, 'get')
+);
+afterEach(() => jest.clearAllMocks())
+
+describe('Testing ReviewService.js', () => {
+
+    it('test call to create review', () => {
+        ReviewService.create('test_review_comment', 5, true, 1, 2);
+        expect(getApiClient.post).toHaveBeenCalledTimes(1);
+        expect(getApiClient.post).toHaveBeenCalledWith('review', {'text':'test_review_comment', 'stars':5, 'owner':true, 'author':1, 'subject':2})
+    });
+
+    it('test getting your own reviews', () => {
+        ReviewService.getMyReviews(1);
+        expect(getApiClient.get).toHaveBeenCalledTimes(1);
+        const params = {subject: 1, author: null}
+        expect(getApiClient.get).toHaveBeenCalledWith('review', {'params':params})
+    });
+})
