fbbc3cffbf3d1fc4ae19d1940d02cde629b74674
matssg <matssg@stud.ntnu.no>
2022-05-06 11:28:37 +0200

    Change "jest.config.js"

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,24 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
-  transformIgnorePatterns: ["/node_modules/(?!@vuepic/)",],
+  transformIgnorePatterns: [
+    "/node_modules/(?!@vuepic/)",
+  ],
+  verbose: false,
+  coveragePathIgnorePatterns: [
+    "node_modules",
+    "http-common.js",
+    "registerServiceWorker.js",
+    "main.js",
+  ],
+
+  /* Test and coverage */
   collectCoverage: true,
-  collectCoverageFrom: ["src/**/*.{js,vue}"],
-  coverageReporters: ['html', 'text', 'text-summary']
+  collectCoverageFrom: [
+    "src/**/*.{js,vue}"
+  ],
+  coverageReporters: [
+    'html', 
+    'text', 
+    'text-summary' /* For GitLab coverage */
+  ]
 }
