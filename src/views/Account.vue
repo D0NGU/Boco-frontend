975666b962e28e2eff635c04b690516a42e9a344
joelmt <joelmt@stud.ntnu.no>
2022-05-06 13:15:21 +0200

    The review icon in the user profile now opens the review tab

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -81,7 +81,7 @@
           <p>Rangering som låner</p>
           <v-rating readonly="" v-model="ratingRenter"></v-rating>
           <br>
-          <v-icon> mdi-message-draw </v-icon> {{reviewsCount}}
+          <v-icon id="reviewCounter" @click="this.tab = 'reviews'"> mdi-message-draw </v-icon> {{reviewsCount}}
         </v-carousel-item>
       </v-carousel>
     </div>
@@ -245,6 +245,9 @@ export default {
 h1 {
   margin-top: 20px;
   margin-bottom: 20px;
+}
+#reviewCounter {
+  cursor: pointer;
 }
 
 </style>
