cd43c1e5160f373b83fe09072dc1316126dabcda
Minh Dan Nguyen <minhdn@stud.ntnu.no>
2022-05-06 09:03:55 +0200

    removed unused code and made minor fixes

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -1,4 +1,4 @@
-<!-- typ "min profil" eller "settings" (endre passord, slett bruker osv) -->
+<!-- Min profil hvor du kan se dine annonser, anmeldelser, endre passord og lignende -->
 
 <template>
   <div id="grid">
@@ -153,13 +153,9 @@ export default {
       this.snackbar = true
       this.edit = false
     },
-    async getNumberOfReviews() {
+    async getReviewData(){
       this.reviewsCount = (await UserAccountService.getNumberOfReviews(this.$store.getters.myUserId)).data;
-    },
-    async getAverageScoreAsOwner() {
       this.ratingSeller = (await UserAccountService.getAverageScoreAsOwner(this.$store.getters.myUserId)).data;
-    },
-    async getAverageScoreAsRenter() {
       this.ratingRenter = (await UserAccountService.getAverageScoreAsRenter(this.$store.getters.myUserId)).data;
     },
     async updateUserDescription() {
@@ -176,13 +172,6 @@ export default {
         this.statusForEditUserDescription = "Noe gikk galt. Prøv igjen"
       }
     },
-
-    async setProfilePic() {
-      await UserAccountService.getUser(this.$store.getters.myUserId).then(response => {
-        this.profilePicSrc = "data:image/jpeg;base64," +response.data.profile64;
-      })
-
-    }
   },
   async beforeMount() {
     let myUserId = this.$store.getters.myUserId;
@@ -197,9 +186,7 @@ export default {
     //get user description
     this.userDescription =  (await UserAccountService.getUserDescription(myUserId)).data
 
-    await this.getNumberOfReviews()
-    await this.getAverageScoreAsOwner()
-    await this.getAverageScoreAsRenter()
+    await this.getReviewData()
   }
 }
 </script>
@@ -208,9 +195,6 @@ export default {
 #grid {
   display: grid;
   height: 400px;
-}
-#components {
-  min-height: 30%;
 }
 #topProfileContainer {
   height: 400px;
@@ -252,9 +236,6 @@ export default {
   width: 100%;
   z-index: 10;
   display: flex;
-}
-.v-card-text {
-  padding: 0;
 }
 
 #userDescription {
